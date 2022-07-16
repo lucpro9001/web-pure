c40daeec7d5f586b2d7dbb5620dc4cb6e9869bb3
lucpro9001 <lucpro9001@gmail.com>
2022-07-16 11:39:51 +0700

    create navbar

--- assets/js/my_js.js
+++ assets/js/my_js.js
@@ -1,10 +1,4 @@
 'use strict';
-var $ = function(component) {
-    var first = component.charAt(0);
-    var name = component.substring(1);
-    return first == '#' ?
-        document.getElementById(name) :
-        first == '.' ?
-        document.getElementsByClassName(name) :
-        document.getElementsByTagName(component);
+var $ = function(selector) {
+    return document.querySelector(selector);
 }
