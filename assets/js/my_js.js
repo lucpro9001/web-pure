32e8c19a8620418218867c47dab8499d1fcb721c
lucpro9001 <lucpro9001@gmail.com>
2022-07-16 21:31:02 +0700

    add scroll smooth and active when scroll

--- assets/js/my_js.js
+++ assets/js/my_js.js
@@ -2,3 +2,28 @@
 var $ = function(selector) {
     return document.querySelector(selector);
 }
+var $$ = function(selector) {
+    return document.querySelectorAll(selector);
+}
+
+let sections = $$('.sec');
+let lists = $$('.nav-item');
+
+function activeLink(nav_item) {
+    lists.forEach((item) => item.classList.remove('active'));
+    nav_item.classList.add('active');
+}
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset - 50 && top < offset + height) {
+            const target = $(`[href='#${id}']`).firstElementChild;
+            activeLink(target);
+        }
+    })
+};
